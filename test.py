73a828f9e01382339ee85bb878295f1bed13a694
eliyahudev <eliyahulevyy@gmail.com>
2026-05-10 12:41:39 -0400

    add a unit test

---
+++ test.py
@@ -0,0 +1,19 @@
+import torch
+from microxcaling.mx.convolution import Conv2d
+
+mx_specs = {
+    "scale_bits": 8,
+    "w_elem_format": "fp6_e3m2",
+    "a_elem_format": "fp6_e3m2",
+    "block_size": 32,
+    "bfloat": 16,
+    "custom_cuda": False,
+    "quantize_backprop": False,
+    "int_ops": True,
+}
+
+conv = Conv2d(32, 8, kernel_size=3, padding=1, mx_specs=mx_specs).cuda()
+x = torch.randn(2, 32, 16, 16, device="cuda")
+
+y = conv(x)
+print(y.shape, y.dtype)
